71cd000eb184defd0d703efaff808614eb97ca6b
Aleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2020-12-10 01:16:40 +0200

    modal 0.2

--- dist/main.js
+++ dist/main.js
@@ -41,13 +41,14 @@ class ModalComponent extends HTMLElement {
             minute: 'numeric',
         };
         const monthDay = date.toLocaleString('en', monthDayOption);
-        const hoursMinutes = date.toLocaleString('en', hoursMinutesOption);
-        // const formattedDate = `${hoursMinutes}, ${monthDay}`;
-        console.log(hoursMinutes, monthDay);
-        // return date.toLocaleString('en', options);
+        const hoursMinutes = date.toLocaleString('en', hoursMinutesOption)
+            .slice(0, 4) + date.toLocaleString('en', hoursMinutesOption)
+            .slice(5).toLowerCase();
+        const formattedDate = `${hoursMinutes}, ${monthDay}`;
+        return formattedDate;
     }
     connectedCallback() {
-        let date = this.getInfoFromDate(this.getCurrDate());
+        let date = this.getFormattedDate(this.getCurrDate());
         console.log('INIT', this);
         this.attachShadow({
             mode: "open"
@@ -58,7 +59,7 @@ class ModalComponent extends HTMLElement {
 
 
             <style>
-                span {
+                p {
                     color: orange;
                 }
 
